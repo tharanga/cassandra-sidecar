fb4de7dd3f3adeaa15b6399d95cd0c03787709c5
Tharanga Gamaethige <tgamaethige@netflix.com>
2019-09-10 23:47:41 -0700

    Added a schema change listener to update CDC reader metadata when DDLs happen in the database.

--- src/main/java/org/apache/cassandra/sidecar/cdc/SSTableDumper.java
+++ src/main/java/org/apache/cassandra/sidecar/cdc/SSTableDumper.java
@@ -1,24 +1,20 @@
 package org.apache.cassandra.sidecar.cdc;
 
-import java.util.Map;
 import java.util.Set;
-
+import java.util.UUID;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.apache.cassandra.db.ColumnFamilyStore;
-import org.apache.cassandra.db.Directories;
 import org.apache.cassandra.db.Keyspace;
 import org.apache.cassandra.db.filter.ColumnFilter;
 import org.apache.cassandra.db.partitions.PartitionUpdate;
 import org.apache.cassandra.db.partitions.UnfilteredPartitionIterator;
-import org.apache.cassandra.io.sstable.Component;
-import org.apache.cassandra.io.sstable.Descriptor;
 import org.apache.cassandra.io.sstable.format.SSTableReader;
 import org.apache.cassandra.schema.Schema;
 import org.apache.cassandra.sidecar.Configuration;
 import org.apache.cassandra.sidecar.cdc.output.Output;
 import org.apache.cassandra.sidecar.cdc.output.OutputFactory;
+
 
 
 
@@ -56,40 +52,44 @@ public class SSTableDumper
             logger.error("Unknown keySpace/columnFamily {}.{}. No data to dump", keySpace, columnFamily);
             return;
         }
-        // TODO: SSTables have index and data files. Index file has partition keys with offsets to the data file. We
-        // can save a LOT of CPU cycles by not de-serializing PartitionUpdate objects. Instead we can read byte offsets
-        // and send them directly to the Kafka.
-        Keyspace ks = Keyspace.openWithoutSSTables(keySpace);
-        ColumnFamilyStore cfs = ks.getColumnFamilyStore(columnFamily);
-        Directories.SSTableLister lister = cfs.getDirectories().sstableLister(null).skipTemporary(true);
-        for (Map.Entry<Descriptor, Set<Component>> sstable : lister.list().entrySet())
-        {
-            if (sstable.getKey() != null)
-            {
-                try
-                {
-                    SSTableReader reader = SSTableReader.open(sstable.getKey());
-                    Keyspace.open(keySpace).getColumnFamilyStore(columnFamily).addSSTable(reader);
-                    if (reader != null)
-                    {
 
-                        UnfilteredPartitionIterator ufp = reader.getScanner();
-                        while (ufp.hasNext())
-                        {
-                            PartitionUpdate partition = PartitionUpdate.fromIterator(ufp.next(),
-                                    ColumnFilter.all(ufp.metadata()));
-                            producer.emitPartition(partition);
-                        }
-                        ufp.close();
-                    }
-                }
-                catch (Throwable t)
+        ColumnFamilyStore cfs = null;
+        String snapshotName = UUID.randomUUID().toString();
+
+        try
+        {
+            // TODO: SSTables have index and data files. Index file has partition keys with offsets to the data file. We
+            // can save a LOT of CPU cycles by not de-serializing PartitionUpdate objects. Instead we can read
+            // byte offsets and send them directly to the Kafka.
+            Keyspace ks = Keyspace.open(keySpace);
+            cfs = ks.getColumnFamilyStore(columnFamily);
+            Set<SSTableReader> sstables = cfs.snapshot(snapshotName, true);
+
+            for (SSTableReader reader : sstables)
+            {
+                if (reader != null)
                 {
-                    logger.error("Couldn't open/read the sstable :  {}", sstable.getKey().filenameFor(Component.DATA));
+                    UnfilteredPartitionIterator ufp = reader.getScanner();
+                    while (ufp.hasNext())
+                    {
+                        PartitionUpdate partition = PartitionUpdate.fromIterator(ufp.next(),
+                                ColumnFilter.all(ufp.metadata()));
+                        producer.emitPartition(partition);
+                    }
+                    ufp.close();
                 }
             }
         }
-
-
+        catch (Exception ex)
+        {
+            logger.error("Couldn't open/read the sstable :  {} : {}", keySpace, columnFamily);
+        }
+        finally
+        {
+            if (cfs != null && cfs.snapshotExists(snapshotName))
+            {
+                cfs.clearSnapshot(snapshotName);
+            }
+        }
     }
 }
